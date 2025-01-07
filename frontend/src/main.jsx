40d7a6480ea2f32d4f01ecb64b4398ddeb6e4343
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-07 23:57:28 +0500

    routes done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "lucide-react";
+import Register from "./Pages/Register.jsx";
+import Login from "./Pages/Login.jsx";
+import Products from "./Pages/Products.jsx";
+import Cart from "./Pages/Cart.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "*",
+        element: (
+          <h1 className="mt-5 p-3 text-red-600 font-semibold flex justify-center items-center">
+            404 | Not Found
+          </h1>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
