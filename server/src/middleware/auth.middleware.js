5cef5384e9a06cb62ac6c8af4ec5ab5234c9ad4e
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-19 16:56:32 +0500

    Working..

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 const authenticateUser = async (res, req, next) => {
-  const token = req.headers["authorization"];
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    res.status(404).json({
-      message: "No token Found",
+    res.status(401).json({
+      message: "No token provided",
     });
     return;
   }
   jwt.verify(token, process.env.ACCESS_JWT_SECRET, (error, user) => {
     if (error) {
       res.status(403).json({
-        message: " Invlaid Token ",
+        message: " Invlaid token ",
       });
       return;
     }
